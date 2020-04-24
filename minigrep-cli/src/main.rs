383eaf12654e4b61c1435865abb75aaf860e7ed1
Oswld <user.oswld@gmail.com>
2020-04-23 21:54:02 -0500

    Init

---
+++ minigrep-cli/src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {} in file {}", query, filename);  
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
+}
