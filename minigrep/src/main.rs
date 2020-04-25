c7cafc541287cc9bf10875e1b75eb77e8bda8867
Oswld <user.oswld@gmail.com>
2020-04-24 19:24:58 -0500

    TDD process

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,14 +7,14 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-         println!("Problem parsing arguments: {}", err);
+    let config = Config::new(&args).unwrap_or_else(|_err| {
+         eprintln!("Problem parsing arguments: {}", _err);
          process::exit(1);
      });
     
 
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+    if let Err(_e) = minigrep::run(config) {
+        eprintln!("Application error: {}", _e);
         process::exit(1);
     }
 }
